2d34fdbc9b1b23e7dec94b8bce908069259f60a3
Anthony <ndegwaanthony300@gmail.com>
2023-10-03 14:54:21 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 99 + 1):
-    if (number <= 0 or number <=9):
-       print("{:02}".format(number), end=",")
+for number in range(0, 100):
+    if (number <= 0 or number <= 9):
+        print("{:02}".format(number), end=",")
     else:
         print(f"{number}", end=",")
