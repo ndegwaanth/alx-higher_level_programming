e8e8eebbc952836d9e34d54fb3f0fde6ffe1511a
Anthony <ndegwaanthony300@gmail.com>
2023-10-03 15:03:24 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for number in range(0, 10):
     for digit in range(number + 1, 10):
         if (number == 8 and digit == 9):
-            print(f"{number}{digit}\n")
+            print(f" {number}{digit}")
         else:
-            print(f"{number}{digit}", end=",")
+            print(f" {number}{digit}", end=",")
