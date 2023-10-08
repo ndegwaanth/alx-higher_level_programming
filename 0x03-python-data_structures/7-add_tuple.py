0974ebbb96a6addb129e0915ebf8bfe9af1bc3f7
Anthony <ndegwaanthony300@gmail.com>
2023-10-08 14:29:42 +0000

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure both tuples have at least 2 elements; use 0 for missing values
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    # Sum the first elements of each tuple and the second elements
+    result = (a[0] + b[0], a[1] + b[1])    
+    return result
