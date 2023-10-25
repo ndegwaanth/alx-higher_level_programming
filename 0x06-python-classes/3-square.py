19230833230373632dcabba65a34baea78bc1170
anthony <ndegwaanthony300@gmail.com>
2023-10-25 22:24:53 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This is based on the the Square"""
+
+
+class Square:
+    """This class class is used to initialize the variable"""
+    def __init__(self, size=0):
+        """Constructor.
+        Args:
+            size(int): This is the length of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ This function calculate the area and return it"""
+        return (self.__size * self.__size)
