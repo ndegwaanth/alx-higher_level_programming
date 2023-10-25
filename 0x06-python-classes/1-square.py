dbb1c7c56ae91f38e5505faa4dbd694173729923
anthony <ndegwaanthony300@gmail.com>
2023-10-25 21:54:42 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This is based on the the Square"""
+
+
+class Square:
+    """This class class is used to initialize the variable"""
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: This is the length of the square
+        """
+        self.__size = size
