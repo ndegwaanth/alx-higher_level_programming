0e6b13b3565a8d9f9a94517151fd582075e9fc7c
anthony <ndegwaanthony300@gmail.com>
2023-11-03 21:08:23 +0000

    0-add_integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this is the implementation of the addition"""
+
+def add_integer(a, b=98):
+    """this funtion  implement the addition of 2 integers.
+       args:
+            a(int): the integer beign passed on.
+            b(int): the second integer beign passed on.
+       raises:
+              TypeError: a must be an integer
+              TypeError: b must be an interger
+    """
+    if (not isinstance(a, int)) and (not isinstance(b, float)):
+        raise TypeError("a must be an integer")
+    if (not isintance(b, float)) and (not isinstance(b, int)):
+        raise TypeError("b must be an integer")
+    return (a + b)
