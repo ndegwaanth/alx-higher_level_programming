d49d094aa61aea0d05617b85a922680d0cc26fde
ndegwaanth <101396314+ndegwaanth@users.noreply.github.com>
2023-11-07 01:33:21 +0300

    files added manually

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,7 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
-    """ this function return the list of the available attribute
-        and methods of the object.
-        args:
-             obj: the value beign passed on
-    """
-    the_attribute_en_methods = dir(obj)
-    return the_attribute_en_methods
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
