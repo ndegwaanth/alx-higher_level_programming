9dba60aeb9a00e1fc1f396a6222baabf82514ec8
anthony <ndegwaanthony300@gmail.com>
2023-11-13 21:42:25 +0000

    modified the files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ this include the python script"""
+
+
 class Base:
     """ this is the first class Base"""
     __nb_object = 0
+
     def __init__(self, id=None):
         """ this is the initializer of the attributes.
             args:
@@ -11,5 +14,5 @@ class Base:
         if id is not None and isinstance(id, int):
             self.id = id
         else:
-            Base.__nb_object +=1
+            Base.__nb_object += 1
             self.id = Base.__nb_object
