fc7c0953dc46516459c9b4ee053725ebc00d20eb
anthony <ndegwaanthony300@gmail.com>
2023-11-13 21:39:50 +0000

    crated the Base.py and __init__.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" this include the python script"""
+class Base:
+    """ this is the first class Base"""
+    __nb_object = 0
+    def __init__(self, id=None):
+        """ this is the initializer of the attributes.
+            args:
+                 @id: this the id
+        """
+        if id is not None and isinstance(id, int):
+            self.id = id
+        else:
+            Base.__nb_object +=1
+            self.id = Base.__nb_object
