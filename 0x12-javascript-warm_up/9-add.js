15282a074de397e3e8750cacd7cd20e40350627c
Anthony <ndegwaanthony300@gmail.com>
2024-02-12 14:28:47 +0300

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (!((isNaN(arg1)) && (isNaN(arg2)))) {
+  console.log(add(arg1, arg2));
+} else {
+  console.log('Nan');
+}
