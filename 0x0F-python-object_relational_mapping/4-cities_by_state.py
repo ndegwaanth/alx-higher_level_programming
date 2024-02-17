206cee35828f78ad35ed6d99a36f43ffcb430a45
anthony <ndegwaanthony300@gmail.com>
2024-02-17 22:01:03 +0000

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    try:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             password=sys.argv[2], database=sys.argv[3],
+                             port=3306)
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+
+        cities = cursor.fetchall()
+
+        for city in cities:
+            print(city)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL: ", e)
+        sys.argv(1)
