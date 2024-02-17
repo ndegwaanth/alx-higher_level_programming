32b47c00b200eccfa4249b8e2362671fc438928a
anthony <ndegwaanthony300@gmail.com>
2024-02-17 21:32:11 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,47 +1,20 @@
 #!/usr/bin/python3
-"""Takes in an argument and display all values in the state
-    table where  name matches the argument
+""" cript that takes in an argument and displays all
+    values in the states table of hbtn_0e_0_usa where
+    name matches the argument.
 """
 import MySQLdb
 import sys
 
 
-def search_states(username, password, database_name, state_name):
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(user=username,
-                             passwd=password,
-                             db=database_name,
-                             host='localhost', port=3306)
-        cursor = db.cursor()
-
-        # Execute the query
-        query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-        cursor.execute(query, (state_name,))
-
-        # Fetch all the rows
-        states = cursor.fetchall()
-
-        # Display the results
-        for state in states:
-            print(state)
-
-        # Close cursor and database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    search_states(username, password, database_name, state_name)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
