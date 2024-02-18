5a0428f134fc28f666d12e33d115ef4f4010af27
anthony <ndegwaanthony300@gmail.com>
2024-02-18 18:16:20 +0000

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-    script that prints the State object with
-    the name passed as argument from the database hbtn_0e_6_usa
+""" prints the State object with the name
+    passed as argument from the database
 """
 import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-if __name == '__main__':
-    if sys.argv != 5:
-        sys.exit(1)
 
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    conn = session.querry(State).filter(state.name == sys.argv[4]).first()
-
-    if conn:
-        print(State.id)
-    else:
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
         print("Not found")
