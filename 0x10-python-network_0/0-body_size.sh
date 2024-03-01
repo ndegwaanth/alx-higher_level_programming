09f0d820fe65baa4a789a753b42e343ba742c378
anthony <ndegwaanthony300@gmail.com>
2024-03-01 14:48:13 +0000

    0-body_size.sh

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the user provided a URL
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Get the URL from the command line argument
+url=$1
+
+# Send a request to the URL using curl and get the size of the response body in bytes
+size=$(curl -s "$url" | wc -c)
+
+# Display the size of the body of the response
+echo "$size"
