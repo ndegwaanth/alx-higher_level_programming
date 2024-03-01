d041cbd6afab23dc7af255d7d13e917b30d11f83
Anthony <ndegwaanthony300@gmail.com>
2024-03-02 00:23:55 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
+#!/usr/bin/python3
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-try:
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body_response = response.read()
         print("Body response:")
         print("\t- type:", type(body_response))
         print("\t- content:", body_response)
         print("\t- utf8 content:", body_response.decode('utf-8'))
-except urllib.error.URLError as e:
-    print("Error accessing URL:", e)
