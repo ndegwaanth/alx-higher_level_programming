f1ed8fe7474c60a68d5a558570a1d01a43d7e9fe
Anthony <ndegwaanthony300@gmail.com>
2024-03-02 01:57:01 +0300

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,13 +2,13 @@
 """ This script sends a request to a URL and displays
     the body of the response.
 """
-
 import requests
 import sys
+
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
     if (response.status_code >= 400):
-        print(f"Error code: {response.status_code}")
+        print("Error code: {}".formart{response.status_code})
     else:
         print(response.text)
