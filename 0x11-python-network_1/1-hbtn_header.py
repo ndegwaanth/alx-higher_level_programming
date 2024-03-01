6972fba50aee6b70de0e28b297d3c830f94209ad
Anthony <ndegwaanthony300@gmail.com>
2024-03-02 00:56:07 +0300

    2-post_email.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,10 +7,6 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
 
     try:
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This script sends a POST request to a URL with an email as a 
+    parameter and displays the body of the response.
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Encode the email parameter
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    try:
+        # Send a POST request with the email parameter
+        with urllib.request.urlopen(url, data=data) as response:
+            # Read and decode the response body
+            body_response = response.read().decode('utf-8')
+        print(f"Your email is: {body_response}")
+    except urllib.error.URLError as e:
+        print("Error accessing URL:", e)
