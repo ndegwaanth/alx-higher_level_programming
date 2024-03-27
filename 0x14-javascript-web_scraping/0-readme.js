e3181b2204f8ae99537cb479277ff4cfa6c39bd7
Anthony <ndegwaanthony300@gmail.com>
2024-03-28 02:07:57 +0300

    initial commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -10,7 +10,6 @@ fs.readFile(process.argv[2], 'utf8', function (error, content) {
   if (error) {
     // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
     console.error('Error reading the file:', error);
-
   } else {
     // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
     console.log(content);
