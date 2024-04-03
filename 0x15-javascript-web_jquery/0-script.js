940683f1a8a853c8f7566320592be7bf4cdcf22e
Anthony <ndegwaanthony300@gmail.com>
2024-04-04 00:25:09 +0300

    1-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,5 @@
-$("document").ready(function() {
-    $("header").css({
-        'color': '#FF0000'
-    });
-});
+// Select the <header> element using document.querySelector
+var header = document.querySelector('header');
+
+// Update the text color to red
+header.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+$("document").ready(function() {
+    $("header").css({
+        'color': '#FF0000'
+    });
+});
