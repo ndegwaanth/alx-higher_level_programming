b2dc9661f081af09b9ed074954a9b01f310a3adf
Anthony Ndegwa <101396314+ndegwaanth@users.noreply.github.com>
2024-05-14 21:10:33 +0300

    Update 5-script.js

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,9 +1,3 @@
-import $ from 'jquery';
-
-$(document).ready(function () {
-  $('#add_item').click(function () {
-    const newItem = $('<li>Item</li>');
-    /* Append the new <li> element to the list with class 'my_list' */
-    $('ul.my_list').append(newItem);
-  });
+$('DIV#add_item').click(function () {
+	('UL.my_list').append('<li>Item</li>');
 });
