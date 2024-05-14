032a9c85e37eff8344d052d5b105eb87c9b442c5
Anthony Ndegwa <101396314+ndegwaanth@users.noreply.github.com>
2024-05-14 21:07:57 +0300

    Update 3-script.js

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,7 +1,3 @@
-import $ from 'jquery';
-
-$('document').ready(function () {
-  $('DIV#red_heade').click(function () {
-    document.getElementsByTagName('header')[0].classList.add('red');
-  });
+$('#red_header').click(function () {
+	$('header').css('color', 'red');
 });
