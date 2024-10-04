28b65ba6b6bfd1b7a71e0955254cb751d24b8008
Anthony <ndegwaanthony300@gmail.com>
2024-10-04 16:44:10 +0300

    updated 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const url = process.argv[2];
 const filename = process.argv[3];
 request(url, function (err, resp, body) {
-    if (resp.statusCode === 200) {
-        fs.writeFile(filename, body, 'utf8', function (err, data) {
-            if (err) return console.log(err);
-        });
-    } else {
-        console.error(err);
-    }
+  if (resp.statusCode === 200) {
+    fs.writeFile(filename, body, 'utf8', function (err, data) {
+      if (err) return console.log(err);
+    });
+  } else {
+    console.error(err);
+  }
 });
